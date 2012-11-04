50faea89d489a293b8c690ef364e6705297f7524
max <max@codehawks.eu>
2012-11-04 15:56:21 +0100

    Add logo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+function resize() {
+	// Logo
+	var logo = document.getElementById('logo');
+	var title = document.getElementById('project_title');
+	
+	if (document.body.offsetWidth > 660 + 2 * 200) {
+		var content = document.getElementById('main_content');
+		var value = content.offsetLeft - logo.offsetWidth;
+
+		logo.style.opacity = 1;
+		logo.style.left = -logo.offsetWidth + "px";
+	} else {
+		logo.style.opacity = 0;
+	}
+}
