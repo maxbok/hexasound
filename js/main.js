57002e734795e22a0f4860080687608c9cb9f8dc
max <max@codehawks.eu>
2012-11-25 07:28:12 +0100

    Use raphaeljs

--- js/main.js
+++ js/main.js
@@ -9,8 +9,12 @@ function load() {
 			$(this).data('hover', false);
 		}
 	).data('hover', false);
-	
+
+	// Load pads
 	loadPads();
+	
+	// Logo
+	loadLogo();
 }
 
 function scroll() {
@@ -95,4 +99,38 @@ function loadPads() {
 			row++;
 		} while((row - 1) * padHeight * 3/4 + verticalOffset < canvas.offsetHeight);
 	};
+}
+
+function loadLogo() {
+	var logoPath = "M 0 60 \
+					A 60 60 0 0 0 120 60 \
+					A 60 60 0 1 0 0 60 \
+					M 60 20 \
+					L 94.6 40 \
+					L 94.6 80 \
+					L 60 100 \
+					L 25.4 80 \
+					L 25.4 40 \
+					Z";
+	
+	var logoDiv = document.getElementById('main_logo');
+	var height = logoDiv.offsetHeight;
+	var paper = Raphael("main_logo", height, height);
+		
+	var scaleRatio = (height - 20) / 120;
+	var offset = ((height - 20) - 120) / 2 + 10;
+		
+	var shadow = paper.path(logoPath);
+	shadow.attr({stroke: "none", fill: "#000", opacity: ".5"});
+	shadow.blur(3);
+	shadow.translate(4 + offset, 4 + offset);
+	shadow.scale(scaleRatio, scaleRatio);
+
+	var logo = paper.path(logoPath);
+	logo.attr({stroke: "none", fill: "90-#184166-#368FE3"});
+	logo.attr({transform: "t" + offset + "," + offset + "s" + scaleRatio + "," + scaleRatio});
+	
+	// var fullLogo = paper.set();
+	// fullLogo.push(shadow, logo);
+	// fullLogo.animate({transform: "t" + offset + "," + offset + "s" + scaleRatio + "," + scaleRatio + "r60"}, 1000, 'bounce');
 }
